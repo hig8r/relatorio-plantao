8165a16d3c6aad7aac2ae29ec503165a9d3811ee
hig8r <124730470+hig8r@users.noreply.github.com>
2026-04-02 13:24:12 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { FilePlus, ClipboardList, Shield } from 'lucide-react';
+import { FilePlus, ClipboardList, Shield, CalendarDays } from 'lucide-react';
 
 export default function Header() {
   const path = usePathname();
